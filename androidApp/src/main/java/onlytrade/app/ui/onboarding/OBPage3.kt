dcc194a128c1b65af94761bc3bdce82627f5b651
Mobin Munir <mmobinbutt@gmail.com>
2024-11-13 04:03:48 +0500

    Completed 3rd onboarding ui design.

--- androidApp/src/main/java/onlytrade/app/ui/onboarding/OBPage3.kt
+++ androidApp/src/main/java/onlytrade/app/ui/onboarding/OBPage3.kt
@@ -1,8 +1,10 @@
 package onlytrade.app.ui.onboarding
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -41,8 +43,7 @@ fun OBPage3(onLoginClick: () -> Unit, onGetStartedClick: () -> Unit) {
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
                     .background(
-                        color = color(0xFFF4FDFA, 0xFF212322),
-                        shape = RoundedCornerShape(16.dp)
+                        color = color(0xFFF4FDFA, 0xFF212322), shape = RoundedCornerShape(16.dp)
                     )
                     .fillMaxWidth()
                     .padding(24.dp)
@@ -50,8 +51,7 @@ fun OBPage3(onLoginClick: () -> Unit, onGetStartedClick: () -> Unit) {
                 AsyncImage(
                     modifier = Modifier.align(Alignment.CenterStart),
                     model = drawableRes(
-                        R.drawable.quickmart_light_intro,
-                        R.drawable.quickmart_dark_intro
+                        R.drawable.quickmart_light_intro, R.drawable.quickmart_dark_intro
                     ),
                     contentScale = ContentScale.None,
                     contentDescription = stringResource(R.string.app_logo)
@@ -77,8 +77,7 @@ fun OBPage3(onLoginClick: () -> Unit, onGetStartedClick: () -> Unit) {
                 modifier = Modifier.padding(vertical = 16.dp),
                 fontSize = 24.sp,
                 color = color(
-                    MaterialTheme.colorScheme.secondary,
-                    MaterialTheme.colorScheme.tertiary
+                    MaterialTheme.colorScheme.secondary, MaterialTheme.colorScheme.tertiary
                 ),
                 text = "Safe and Secure\nDeals",
 
@@ -91,22 +90,50 @@ fun OBPage3(onLoginClick: () -> Unit, onGetStartedClick: () -> Unit) {
                 text = " OnlyTrade provides full scale arbitration\nand customer support for your\ntrusted trades and offers."
             )
 
-            Button(
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp), onClick = {
-
-
-                }, shape = RoundedCornerShape(8.dp), colors = ButtonDefaults.buttonColors(
-                    containerColor = color(
-                        MaterialTheme.colorScheme.secondary,
-                        Color(0xFF21D4B4)
-                    )
-                )
+                ,
             ) {
-                Text("Next", color = Color.White, modifier = Modifier.padding(vertical = 8.dp))
-            }
+                Button(
+                    modifier = Modifier
+                        .weight(.5f)
+                        .padding(8.dp)
+                        .border(
+                            2.dp,
+                            color = color(0xFFF4F5FD, 0xFF282828),
+                            RoundedCornerShape(8.dp)
+                        ),
+                    onClick = onLoginClick,
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(
+                        "Login",
+                        color = color(0xFF1C1B1B, 0xFF21D4B4),
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+                }
 
+                Button(
+                    modifier = Modifier
+                        .weight(.5f)
+                        .padding(8.dp),
+                    onClick = onGetStartedClick,
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = color(
+                            MaterialTheme.colorScheme.secondary, Color(0xFF21D4B4)
+                        )
+                    )
+                ) {
+                    Text(
+                        "Get Started",
+                        color = Color.White,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+                }
+
+            }
         }
 
     }
